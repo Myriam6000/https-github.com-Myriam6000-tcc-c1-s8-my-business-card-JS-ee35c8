396056997c454503db63f5f17dd12ddad704e60a
Myriam6000 <myriam.forgeron@gmail.com>
2019-05-31 15:22:13 +0200

    first commit business_card and objects in JS

---
+++ business_card.js
@@ -0,0 +1,11 @@
+business_card = {firstname:"Myriam", 
+                lastname:"Forgeron", 
+                age: 48, 
+                town:"Ferney",
+                email:"myriam.forgeron@gmail.com",
+                learnerID:"ee35c8"}
+
+console.log(business_card.firstname);
+console.log(business_card.email);
+console.log(business_card["town"]);
+console.log(business_card.learnerID);
